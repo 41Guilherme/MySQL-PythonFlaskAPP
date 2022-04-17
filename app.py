82bd6ade0a0d2160b8a468e79fbba91bd3e81236
41Guilherme <guilherme41@usp.br>
2022-04-17 17:47:27 -0300

    Correção de Bug na inserção de usuario

--- app.py
+++ app.py
@@ -21,11 +21,14 @@ def send_user():
     New = request.get_json()
     data = DB.getAllData()
     index = len(data) -1
+    if (len(data) <= 0):
+        DB.insertItem(1, str(New['nome']), int(New['idade']))
+        return jsonify(New), 201
     id = data[index]['id'] + 1
     
     DB.insertItem(int(id), str(New['nome']), int(New['idade']))
     
-    return jsonify(New), 200
+    return jsonify(New), 201
     
     
 if __name__ == "__main__":
